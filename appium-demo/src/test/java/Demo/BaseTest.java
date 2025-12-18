331d92895061915aa2efb94256019cd0e9802d4e
Ompokar <pokarom203@gmail.com>
2025-12-18 11:54:30 +0530

    Fix import statement formatting in BaseTest.java

--- appium-demo/src/test/java/Demo/BaseTest.java
+++ appium-demo/src/test/java/Demo/BaseTest.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 import java.lang.reflect.Method;
 
 import org.testng.ITestResult;
-import org.testng.annotations.*;n
+import org.testng.annotations.*;
 
 import com.aventstack.extentreports.*;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
